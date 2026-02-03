52dba6c5eb56ea3d61b6b3a5a8eb983fccfd5c2b
chewndaeng <ohg0806@gmail.com>
2026-02-03 20:28:02 +0900

    Update 포켓몬_오한결.py

--- [DAY6] LV1_포켓몬/포켓몬_오한결.py
+++ [DAY6] LV1_포켓몬/포켓몬_오한결.py
@@ -1,14 +1,4 @@
-def solution(number, limit, power):
-    cnt = [0] * (number + 1)
-
-    for k in range(1, number + 1):
-        for m in range(k, number + 1, k):
-            cnt[m] += 1
-
-    total = 0
-    for i in range(1, number + 1):
-        if cnt[i] > limit:
-            total += power
-        else:
-            total += cnt[i]
-    return total
+def solution(nums):
+    n = len(nums)
+    unique = len(set(nums))
+    return min(unique, n // 2)
