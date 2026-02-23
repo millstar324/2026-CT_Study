007363898d4eda0e474474e45a6c407abbf29465
millstar324 <schaewon324@gmail.com>
2026-02-23 21:07:59 +0900

    [6] millstar 백준-소팅알고리즘

---
+++ millstar/[ST]백준_수정렬하기2_2751.py
@@ -0,0 +1,20 @@
+#start: 8:56
+#end: 
+
+import sys
+
+def solve():
+    n = int(sys.stdin.readline().strip())
+    arr = []
+
+    for _ in range(n):
+        m = int(sys.stdin.readline().strip())
+        arr.append(m)
+    arr.sort()
+    for i in arr:
+        print(i)
+
+solve()
+    
+
+
